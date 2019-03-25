6cefc7247ddb42eacb2b7d3ed0a072f131b09404
Stanislaw Huell <huell@stud.uni-heidelberg.de>
2019-03-25 11:35:16 +0100

    finish further-consideration

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,6 +4,7 @@ c3sr_base="/home/shuell/Projects/c3srmatrix/"
 this_base="/home/shuell/Files/Studium/MSC_TI/WS18-19/Masterarbeit/student-research-project/"
 assets_dir=${this_base}"assets/"
 
-# Copy C3SR arithmetic benchmark assets
+# Copy C3SR arithmetic and compression benchmark assets
 cp ${c3sr_base}benchmark/arithmetic/analysis/img/*.png ${assets_dir}
+cp ${c3sr_base}benchmark/compression/structured_grid_heap_size/analysis/img/*.png ${assets_dir}
 
